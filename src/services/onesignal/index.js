4854b79769b9e5169177791aed47e3b1c7c0474b
Gabriel Araujo <gabriiel66@gmail.com>
2020-11-09 00:09:33 -0300

    feat: onesignal

--- src/services/onesignal/index.js
+++ src/services/onesignal/index.js
@@ -1,6 +1,4 @@
-import OneSignal, { useOneSignalSetup } from 'react-onesignal'
+import OneSignal from 'react-onesignal'
 import config from './config'
-
-export useOneSignalSetup
 
 export default OneSignal.initialize(config.appId)
